d1c894988b21c007353e5b9c2205533af3e86491
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2023-02-12 02:11:20 +0100

    Initial commit

---
+++ src/main/java/com/springframework/spring6webapp/domain/Author.java
@@ -0,0 +1,11 @@
+package com.springframework.spring6webapp.domain;
+/*
+ * @Project Name: spring-6-webapp
+ * @Author: Okechukwu Bright Onwumere
+ * @Created: 12/02/2023
+ */
+
+
+public class Author {
+
+}
